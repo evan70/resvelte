dcddaa827f5ec8ad68791f45715727f51b701c4c
evan70 <ivanpaldan@yandex.com>
2023-04-02 20:34:39 +0200

    new file:   _ide_helper.php 	new file:   _ide_helper_actions.php

---
+++ _ide_helper_actions.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Actions\Admin;
+
+/**
+ */
+class Dashboard
+{
+}
+namespace App\Actions\Auth;
+
+/**
+ */
+class Login
+{
+}
+/**
+ */
+class Register
+{
+}
+namespace App\Actions\Frontend;
+
+/**
+ */
+class About
+{
+}
+/**
+ */
+class Contact
+{
+}
+/**
+ */
+class Home
+{
+}
+namespace App\Actions\User;
+
+/**
+ */
+class Dashboard
+{
+}
+namespace Lorisleiva\Actions\Concerns;
+
+/**
+ * @method void asController()
+ */
+trait AsController
+{
+}
+/**
+ * @method void asListener()
+ */
+trait AsListener
+{
+}
+/**
+ * @method void asJob()
+ */
+trait AsJob
+{
+}
+/**
+ * @method void asCommand(\Illuminate\Console\Command $command)
+ */
+trait AsCommand
+{
+}
