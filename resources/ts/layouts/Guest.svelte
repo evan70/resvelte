ce30ed9cb28c416450edfd175368031398a8abfd
evan70 <ivanpaldan@yandex.com>
2023-04-04 19:28:57 +0200

    modified:   resources/ts/layouts/Guest.svelte

--- resources/ts/layouts/Guest.svelte
+++ resources/ts/layouts/Guest.svelte
@@ -1,22 +1,24 @@
-<script>import ApplicationLogo from '@/components/layout/ApplicationLogo.svelte'
+<script lang='ts'>
+import ApplicationLogo from '@/components/layout/ApplicationLogo.svelte'
 import Link from '@/components/shared/Link.svelte'
 </script>
 
 <svelte:body></svelte:body>
+
 <div
-  class=' min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100  '>
+    class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900"
+>
+    <div>
+        <Link href="/">
+            <ApplicationLogo classes="w-20 h-20 fill-current text-gray-500" />
+        </Link>
+    </div>
 
-  <div>
-    <Link href='/'>
-      <ApplicationLogo class='w-20 h-20 fill-current text-gray-500' />
-    </Link>
-  </div>
-
-  <div
-    class='w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg'>
-    
-    <slot />
-  </div>
+    <div
+        class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
+    >
+        <slot />
+    </div>
 </div>
 
 
